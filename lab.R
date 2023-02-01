0cf44f452cb1d6090e12498db9aff620d18062e9
bella <bella@bellalee.com>
2023-02-01 11:19:35 -0800

    Complete both sections

--- lab.R
+++ lab.R
@@ -9,62 +9,83 @@
 ## Use an appropriate variable name for the scores
 ## 
 ## Hint: feel free to invent if you cannot figure this out
-
+seahawks_points <- c(10, 13, 24, 27, 34)
+seahawks_points
 
 ## Create a vector of the number of points the opponent
 ## scored against Seahawks in the first 5 games
 ## use an appropriate variable name
-
+opponent_points <- c(24, 21, 30, 23, 40)
+opponent_points
 
 ## Combine your two vectors into a dataframe
-
+scores <- data.frame(seahawks_points, opponent_points)
 
 ## Create a new column "diff" that is the difference in points
 ## (in favor of Hawks)
-
+scores$diff <- seahawks_points - opponent_points
 
 ## Create a new column "won" which is TRUE if the Seahawks won,
 ## ie if Seahawks scored more than the opponent scored against them
-
+scores$won <- scores$diff > 0
 
 ## Create a vector of the opponents name (such as "Denver Broncos")
-
+opponents <- c("Chiefs", "49ers", "Panthers", "Rams", "Raiders")
 
 ## Add the vector of opponents into the data frame
-
+scores$opponents <- opponents
+View(scores)
 
 ## Compute the average score of Seahawks
-
+seahawks_avg <- mean(scores$seahawks_points)
+seahawks_avg
 
 ## Compute how many games did Seahawks won
 ## (use the 'won' variable to compute it)
-
+wins <- scores[scores$won == TRUE, ]
+num_wins <- nrow(wins)
+num_wins
 
 ## What was the largest difference in scores (in favor of Seahawks)?
-
+largest_win <- max(scores$diff)
+largest_win
 
 ## How many different opponents did Seahawks have in these games?
 ## Hint: use `unique()` and `length()`
-
+unique_opponents <- unique(scores$opponents)
+num_opponents <- length(unique_opponents)
 
 ## Print the variable names in your data frame
-
+names(scores)
 
 ## Write a loop over all variables in your data frame
 ## print the variable name inside the loop
-
+for (name in names(scores)) {
+  print(name)
+}
 
 ## Write a loop over all variables in your data frame
 ## print the variable name inside the loop,
 ## and true/false, depending if the variable is numeric
 ## check out 'is.numeric()'
-
+for(name in names(scores)) {
+  is_num <- is.numeric(scores[, name])
+  cat(name, is_num, "\n")
+}
 
 ## Write a loop over all variables in your data frame
 ## print the variable name inside the loop,
 ## and the average value of the variable
 ## if the variable is numeric
-
+for(name in names(scores)) {
+  is_num <- is.numeric(scores[, name])
+  if (is_num) {
+    avg <- mean(scores[, name])
+  } else {
+    avg <- "N/A"
+  }
+  cat(name, avg, "\n")
+}
 
 ##
 ## HR data
